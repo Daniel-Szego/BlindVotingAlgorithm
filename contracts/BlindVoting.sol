8243f00febeab4d1500c73c7546e814850d63122
Daniel <szego.daniel@gmail.com>
2018-11-14 13:05:31 -0800

    compilable version still not running hundred percent

--- contracts/BlindVoting.sol
+++ contracts/BlindVoting.sol
@@ -5,15 +5,17 @@ contract BlindVoting {
   enum VotingStates {Init, Voting, Counting, Closed }
   VotingStates public votingState;
   string public version = "0.0.1";
-  uint public from;
-  uint public to;
+  int public from;
+  int public to;
   address public owner;
+  string private intCalc;
   
   address[] participants;
   mapping(address => string) public blindVotes;
+  mapping(int => int) votes;
 
   // new contract -> new voting round -> voting on numbers
-  constructor(uint _from, uint _to){
+  constructor(int _from, int _to){
       from = _from;
       to = _to;
       owner = msg.sender;
@@ -23,7 +25,7 @@ contract BlindVoting {
   // EVENTS
   event VotingStarted();
   event CountingStarted();
-  event VotingClosed(); 
+  event VotingClosed(int256 result); 
 
   // MODIFIERS
   // modifier : can be executed only by the owner
@@ -64,13 +66,22 @@ contract BlindVoting {
   }
 
   function finishVoting() inCounting() isOwner() {
+      // counting votes
+      int result = -1;
+      int highestVoteNumber = -1;
+      for (int i = from; i <= to; i++){
+          if (votes[i] > highestVoteNumber){
+             highestVoteNumber = votes[i];
+             result = i;
+          }
+      }
+      // updating state
       votingState = VotingStates.Closed;
-      emit VotingClosed();
+      emit VotingClosed(result);
   }
 
-
   // BASIC FUNCTIONS
-  
+  // voting based on a sha256 value of the number and the salt
   function vote(string blindVote) inVoting() public {
     if (bytes(blindVotes[msg.sender]).length == 0){  
           blindVotes[msg.sender] = blindVote;
@@ -78,8 +89,31 @@ contract BlindVoting {
     }
   }
 
-
-  function revealVote(uint vote, string salt) inCounting() public {
-
+  // revealing the vote in the voting round
+  function revealVote(int vote, string salt) inCounting() public {
+      string blindVote = blindVotes[msg.sender];
+      if (compareStrings(blindVote,intCalc)) {
+          votes[vote] = votes[vote] + 1;
+      }     
   }
+
+function compareStrings (string a, string b) view returns (bool){
+        bytes32  aa = stringToBytes32(a);
+        bytes32  bb = stringToBytes32(b);
+        bytes32 x = aa[1];
+        bytes32 z = bb[1];
+        
+       return x == z;
+ }
+
+function stringToBytes32(string memory source) returns (bytes32 result) {
+    bytes memory tempEmptyStringTest = bytes(source);
+    if (tempEmptyStringTest.length == 0) {
+        return 0x0;
+    }
+
+    assembly {
+        result := mload(add(source, 32))
+    }
+}
 }
