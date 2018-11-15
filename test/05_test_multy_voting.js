d3dab1ca2bb961242e9baf8cb136f9387d693705
Daniel <szego.daniel@gmail.com>
2018-11-15 06:50:55 -0800

    multiply voting working

--- test/05_test_multy_voting.js
+++ test/05_test_multy_voting.js
@@ -4,7 +4,8 @@ var secondState;
 var thirdState;
 var fourthState;
 var winnerValue;
-var hashInput = 'AD57366865126E55649ECB23AE1D48887544976EFEA46A48EB5D85A6EEB4D306';
+var hashInput100 = 'AD57366865126E55649ECB23AE1D48887544976EFEA46A48EB5D85A6EEB4D306';
+var hashInput99 = '8C1F1046219DDD216A023F792356DDF127FCE372A72EC9B4CDAC989EE5B0B455';
 var hashOutput;
 
 contract('Voting', function(accounts) {
@@ -19,11 +20,15 @@ contract('Voting', function(accounts) {
             return VotingInstance.votingState({from: accounts[0]});             
         }).then(function(result) {
             secondState = result;
-            return VotingInstance.vote(hashInput, {from: accounts[0]});             
+            return VotingInstance.vote(hashInput100, {from: accounts[0]});             
         }).then(function(result) {
             return VotingInstance.blindVotes(accounts[0], {from: accounts[0]});             
         }).then(function(result) {
             hashOutput = result;
+            return VotingInstance.vote(hashInput99, {from: accounts[1]});             
+        }).then(function(result) {
+            return VotingInstance.vote(hashInput99, {from: accounts[2]});             
+        }).then(function(result) {    
             return VotingInstance.startCountingRound({from: accounts[0]});                          
         }).then(function(result) {
             return VotingInstance.votingState({from: accounts[0]});             
@@ -31,6 +36,10 @@ contract('Voting', function(accounts) {
             thirdState = result;            
             return VotingInstance.revealVote(100,"", {from: accounts[0]});        
         }).then(function(result) {   
+            return VotingInstance.revealVote(99,"", {from: accounts[1]});        
+        }).then(function(result) {   
+            return VotingInstance.revealVote(99,"", {from: accounts[2]});        
+        }).then(function(result) {               
             return VotingInstance.finishVoting({from: accounts[0]});                          
         }).then(function(result) {  
             return VotingInstance.votingState({from: accounts[0]}); 
@@ -43,8 +52,8 @@ contract('Voting', function(accounts) {
             assert.equal(secondState, 1, "second state Voting");             
             assert.equal(thirdState, 2, "third state Counting");             
             assert.equal(fourthState, 3, "fourth state Counting");             
-            assert.equal(hashOutput, hashInput, "hash input and outputs are the same");             
-            assert.equal(winnerValue, 100, "winner is 100");                         
+            assert.equal(hashOutput, hashInput100, "hash input and outputs are the same");             
+            assert.equal(winnerValue, 99, "winner is 100");                         
         });
     });
 });
