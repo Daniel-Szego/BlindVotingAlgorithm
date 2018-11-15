e544c5372556d078e523d8f2d42ff862b03811c0
Daniel <szego.daniel@gmail.com>
2018-11-15 05:39:50 -0800

    simple voting functionality tested

--- test/03_test_voting.js
+++ test/03_test_voting.js
@@ -1,16 +1,16 @@
 var Voting = artifacts.require("BlindVoting");
 var firstState;
 var secondState;
-var hashInput = "AD57366865126E55649ECB23AE1D48887544976EFEA46A48EB5D85A6EEB4D306";
+var hashInput = 'AD57366865126E55649ECB23AE1D48887544976EFEA46A48EB5D85A6EEB4D306';
 var hashOutput;
 
 contract('Voting', function(accounts) {
     it("test of the voting functionality", function() {
         return Voting.deployed(10, 100).then(function(instance) {
             VotingInstance = instance;
-            return VotingInstance.startVotingRound({from: accounts[0]});             
+            return VotingInstance.votingState({from: accounts[0]});                         
         }).then(function(result) {
-            firstState = result;
+            firstState = result;            
             return VotingInstance.startVotingRound({from: accounts[0]});             
         }).then(function(result) {
             return VotingInstance.votingState({from: accounts[0]});             
